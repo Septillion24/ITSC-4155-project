05c6fa0d53017424f222265f322d309fa22209d4
Antisocial-Gamer <james83701@gmail.com>
2024-10-31 17:31:01 -0400

    Update MuscleGroup.ts

--- src/classes/MuscleGroup.ts
+++ src/classes/MuscleGroup.ts
@@ -1,9 +1,10 @@
 export default class MuscleGroup {
 	muscleGroupId: number;
 	name: string;
-
-	constructor(muscleGroupId: number, name: string) {
+	exerciseIds: number[];
+	constructor(muscleGroupId: number, name: string, exerciseIds: number[]) {
 		this.muscleGroupId = muscleGroupId;
 		this.name = name;
+		this.exerciseIds = exerciseIds;
 	}
 }
