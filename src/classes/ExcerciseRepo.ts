24e45dfb48bccc03b08b241b728f99307d6f3953
Antisocial-Gamer <james83701@gmail.com>
2024-10-31 17:32:23 -0400

    Revert "testing?"
    This reverts commit ec010f5c06fa959c5bdc3f3609e20078bcf823c1.

--- src/classes/ExcerciseRepo.ts
+++ src/classes/ExcerciseRepo.ts
@@ -1,4 +1,4 @@
-import sql from '../lib/DatabaseConnection';
+import sql from '$lib/DatabaseConnection';
 import Exercise from './Exercise';
 
 export default class ExerciseRepo {
--- src/lib/databaseConnection.ts
+++ src/lib/databaseConnection.ts
@@ -1,14 +1,5 @@
 import postgres from 'postgres';
-import dotenv from 'dotenv';
-dotenv.config();
-console.log(process.env);
-
-const { DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD } = process.env;
-
-if (!DATABASE_URL) {
-	console.log('DATABASE_URL:', DATABASE_URL);
-	throw new Error('DATABASE_URL is not defined');
-}
+import { DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD } from '$env/static/private';
 
 const sql = postgres(DATABASE_URL, {
 	username: DATABASE_USERNAME, // Username of database user
