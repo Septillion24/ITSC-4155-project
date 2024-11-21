dfa7666b344c185d0355def911d8e2315952ddb0
Antisocial-Gamer <james83701@gmail.com>
2024-11-20 20:10:19 -0500

    edited api call

--- src/routes/api/get/exerciseStats/+server.ts
+++ src/routes/api/get/exerciseStats/+server.ts
@@ -3,7 +3,8 @@ import { SessionManager } from '../../../../classes/SessionManager.js';
 export async function GET({ cookies }): Promise<Response> {
 	const token = cookies.get('token');
 	if (!token) return new Response('Unauthorized', { status: 401 });
-	const userID = SessionManager.getUserFromUUID(token);
-	const exerciseStats = ExerciseStatRepo.getExerciseStatsByUser(userID);
+	const user = await SessionManager.getUserFromUUID(token);
+	if (user === null) return new Response('No user found', { status: 404 });
+	const exerciseStats = ExerciseStatRepo.getExerciseStatsByUser(user.id);
 	return new Response(JSON.stringify(exerciseStats), { status: 200 });
 }
